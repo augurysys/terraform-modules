d892425497e4d4f463805386a9eebae36fdd1c3f
Vitaly Gorodetsky <gorodetsky@gmail.com>
2018-12-29 17:21:54 +0200

    adds scylladb_addresses output

--- gcs-scylladb-cluster/outputs.tf
+++ gcs-scylladb-cluster/outputs.tf
@@ -1,3 +1,7 @@
 output "scylladb_seeds" {
   value = "${join(",", slice(google_compute_address.scylladb.*.address, 0, 2))}"
 }
+
+output "scylladb_addresses" {
+  value = "${google_compute_address.scylladb.*.address}"
+}
