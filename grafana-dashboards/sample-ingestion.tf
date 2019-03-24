ad6c5dd08f049cc5fbb19833a97ed4bfda4d8998
Vitaly Gorodetsky <gorodetsky@gmail.com>
2019-03-24 14:11:27 +0200

    adds sample-ingestion dashboard

---
+++ grafana-dashboards/sample-ingestion.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_config_map" "sample_ingestion_dashboard" {
+  metadata {
+    name      = "sample-ingestion"
+    namespace = "monitoring"
+
+    labels = {
+      app = "prometheus-operator-grafana"
+      grafana_dashboard = "1"
+      release = "prometheus-operator"
+    }
+  }
+
+  data {
+    "sample-ingestion.json" = "${file("${path.module}/templates/sample-ingestion-dashboard.json")}"
+  }
+}
