09fd1429ad480457267d513afc3e1b0a1a519206
Vitaly Gorodetsky <gorodetsky@gmail.com>
2019-04-17 13:11:23 +0300

    adds k8s-deployments-dashboard

---
+++ grafana-dashboards/k8s-deployments-dashboard.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_config_map" "k8s_deployments_dashboard" {
+  metadata {
+    name      = "k8s-deployments-dashboard"
+    namespace = "monitoring"
+
+    labels = {
+      app               = "prometheus-operator-grafana"
+      grafana_dashboard = "1"
+      release           = "prometheus-operator"
+    }
+  }
+
+  data {
+    "k8s-deployments.json" = "${file("${path.module}/templates/k8s-deployments-dashboard.json")}"
+  }
+}
