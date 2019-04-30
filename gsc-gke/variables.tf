45628d58492138b8156d3f62aa139a6b9b59b4a4
Vitaly Gorodetsky <gorodetsky@gmail.com>
2019-04-30 12:17:16 +0300

    removes node pools from GKE module

--- gsc-gke/variables.tf
+++ gsc-gke/variables.tf
@@ -27,67 +27,6 @@ variable "gke_master_password" {
   default = ""
 }
 
-variable "autoscaling_min" {
-  default = 0
-}
-
-variable "autoscaling_max" {
-  default = 1
-}
-
-variable "algo_node_pool_enabled" {
-  default = "true"
-}
-
-variable "algo_autoscaling_min" {
-  default = 0
-}
-
-variable "algo_autoscaling_max" {
-  default = 1
-}
-
-variable "machine_type" {
-  default = "n1-standard-2"
-}
-
-variable "algo_machine_type" {
-  default = "n1-highmem-2"
-}
-
-variable "local_ssd_count" {
-  default = 1
-}
-
-variable "disk_size_gb" {
-  default = 300
-}
-
-variable "algo_disk_size_gb" {
-  default = 300
-}
-
-variable "auth_scops" {
-  default = [
-    "https://www.googleapis.com/auth/compute",
-    "https://www.googleapis.com/auth/devstorage.read_only",
-    "https://www.googleapis.com/auth/logging.write",
-    "https://www.googleapis.com/auth/monitoring",
-  ]
-}
-
-variable "taint" {
-  type = "list"
-
-  default = [
-    {
-      key    = "cloud.google.com/gke-preemptible"
-      value  = "true"
-      effect = "NO_SCHEDULE"
-    },
-  ]
-}
-
 variable "master_ipv4_cidr_block" {}
 variable "cluster_ipv4_cidr_block" {}
 variable "services_ipv4_cidr_block" {}
