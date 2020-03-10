649476a6e92e9e1bdeb98f732d0fb5f154823319
guycarmy <guycarmy@gmail.com>
2020-03-10 11:04:49 +0200

    Create module for service accounts for microservices

---
+++ service-account-for-microservice/main.tf
@@ -0,0 +1,18 @@
+resource "google_service_account" "service_account" {
+  account_id   = var.id
+  display_name = var.name
+}
+
+resource "google_service_account_key" "service_account_key" {
+  service_account_id = google_service_account.service_account.name
+}
+
+resource "kubernetes_secret" "service_account_secret" {
+  metadata {
+    name = "${var.id}-service-account"
+  }
+
+  data = {
+    "service-account.json" = base64decode(google_service_account_key.service_account_key.private_key)
+  }
+}
---
+++ service-account-for-microservice/variables.tf
@@ -0,0 +1,9 @@
+variable id {
+  description = "The account_id for the service account"
+  type        = string
+}
+
+variable name {
+  description = "The display_name for the service account"
+  type        = string
+}
