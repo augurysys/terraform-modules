f66b5b7e9c7ffccc8fc7071410382677919fa824
Vitaly Gorodetsky <gorodetsky@gmail.com>
2020-04-16 13:52:40 +0300

    fixes scylladb_seeds

--- gcs-scylladb-cluster/outputs.tf
+++ gcs-scylladb-cluster/outputs.tf
@@ -1,5 +1,5 @@
 output "scylladb_seeds" {
-  value = join(",", slice(google_compute_address.scylladb.*.address, 0, 2))
+  value = join(",", slice(google_compute_address.scylladb.*.address, 0, (var.cluster_size < 3 ? var.cluster_size - 1 : 2)))
 }
 
 output "scylladb_addresses" {
