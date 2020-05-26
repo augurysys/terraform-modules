52da0e797434cae5c36e92b04cdd1be33016ce29
Vitaly Gorodetsky <gorodetsky@gmail.com>
2020-05-26 11:04:41 +0300

    updates scylla dashboards

--- grafana-dashboards/scylla-dashboards.tf
+++ grafana-dashboards/scylla-dashboards.tf
@@ -11,7 +11,13 @@ resource "kubernetes_config_map" "scylla_dashboards" {
   }
 
   data = {
-    "scylla-dash.3.0.json"    = file("${path.module}/templates/scylla-dash.3.0.json")
-    "scylla-manager.1.3.json" = file("${path.module}/templates/scylla-manager.1.3.json")
+    "scylla-cpu.2019.1.json"    = file("${path.module}/templates/scylla-cpu.2019.1.json")
+    "scylla-cql.2019.1.json" = file("${path.module}/templates/scylla-cql.2019.1.json")
+    "scylla-detailed.2019.1.json" = file("${path.module}/templates/scylla-detailed.2019.1.json")
+    "scylla-errors.2019.1.json" = file("${path.module}/templates/scylla-errors.2019.1.json")
+    "scylla-io.2019.1.json" = file("${path.module}/templates/scylla-io.2019.1.json")
+    "scylla-os.2019.1.json" = file("${path.module}/templates/scylla-os.2019.1.json")
+    "scylla-overview.2019.1.json" = file("${path.module}/templates/scylla-overview.2019.1.json")
+    "scylla-manager.2.0.json" = file("${path.module}/templates/scylla-manager.2.0.json")
   }
 }
