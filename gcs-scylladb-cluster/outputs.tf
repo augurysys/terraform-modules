764233749bf2e5df64260925d548f24f2f62e29d
Vitaly Gorodetsky <gorodetsky@gmail.com>
2020-07-02 15:10:31 +0300

    add scylladb_nodes output

--- gcs-scylladb-cluster/outputs.tf
+++ gcs-scylladb-cluster/outputs.tf
@@ -2,6 +2,6 @@ output "scylladb_seeds" {
   value = join(",", slice(google_compute_address.scylladb.*.address, 0, (var.cluster_size < 3 ? var.cluster_size - 1 : 2)))
 }
 
-output "scylladb_addresses" {
-  value = google_compute_address.scylladb.*.address
+output "scylladb_nodes" {
+  value = join(",", google_compute_address.scylladb.*.address)
 }
