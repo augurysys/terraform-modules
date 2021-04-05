057b2406a7680601140156fd4459131af1c3444c
guycarmy <guycarmy@gmail.com>
2021-04-05 14:43:40 +0300

    Add enable_private_endpoint to gke

--- gsc-gke/main.tf
+++ gsc-gke/main.tf
@@ -11,6 +11,7 @@ resource "google_container_cluster" "default" {
 
   private_cluster_config {
     enable_private_nodes   = true
+    enable_private_endpoint = false
     master_ipv4_cidr_block = var.master_ipv4_cidr_block
   }
 
