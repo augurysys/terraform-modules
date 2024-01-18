ff79903ad9acf7bbff14a0a0d5db7da969bf933d
galrami <76769997+galrami@users.noreply.github.com>
2024-01-18 12:33:01 +0200

    Update main.tf

--- service-account-for-microservice/main.tf
+++ service-account-for-microservice/main.tf
@@ -7,12 +7,12 @@ resource "google_service_account_key" "service_account_key" {
   service_account_id = google_service_account.service_account.name
 }
 
-resource "kubernetes_secret" "service_account_secret" {
-  metadata {
-    name = "${var.id}-service-account"
-  }
-
-  data = {
-    "service-account.json" = base64decode(google_service_account_key.service_account_key.private_key)
-  }
-}
+#resource "kubernetes_secret" "service_account_secret" {
+#  metadata {
+#    name = "${var.id}-service-account"
+#  }
+#
+#  data = {
+#    "service-account.json" = base64decode(google_service_account_key.service_account_key.private_key)
+#  }
+#}
